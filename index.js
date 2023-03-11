d9d8e0c3b3aab52736547030ee9883e8035dd635
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-11 23:23:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
-function writeCards(names, event) {
-  const cards = ["Guadalupe", "Ollie" ,"Aki" ];
-  for(let i = 0; i < names.length; i++) {
-  
-    cards.push('Thank you,${names[i]}, forthe wonderful ${event} gift!');
-  }
-  return cards;
-}
-function WriteCards(names, event) {
-  let messages = true;
+let name = ["Guadalupe", "Ollie", "Aki"];
+let event = ["Surprise"]
+function writeCards(names, events) {
+  const message = []
   for (let i = 0; i < names.length; i++) {
-    let message = 'Thank you,${name[i]}, for the wonderful ${event} gift!'
-     messages.push(message);
+    message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
   }
-  return messages
+  
+  return message;
 }
-
 
 function countDown(number) {
   while (number >= 0) {
